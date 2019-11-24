111daafdb50b1f0f7c09f15e585d91fb98431b85
Ahmed Hadjou <thehidjou@yahoo.com>
2019-11-24 14:10:55 +0000

    deployment changes

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const { MONGODB } = require('./config.js');
 
 const pubsub = new PubSub();
 
+const PORT = process.env.port || 5000;
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -17,8 +19,11 @@ mongoose
   .connect(MONGODB, { useNewUrlParser: true })
   .then(() => {
     console.log('MongoDB Connected');
-    return server.listen({ port: 5000 });
+    return server.listen({ port: PORT });
   })
   .then((res) => {
     console.log(`Server running at ${res.url}`);
-  });
+  })
+  .catch(err => {
+    console.error(err)
+  })
